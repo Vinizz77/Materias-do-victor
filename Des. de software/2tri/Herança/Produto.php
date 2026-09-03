70e7200ee5c4166fe732befa1b151ca7ed970122
Vinicius Lima <vl3648570@gmail.com>
2026-09-02 21:10:04 -0300

    atualização de casa

--- Des. de software/2tri/Herança/Produto.php
+++ Des. de software/2tri/Herança/Produto.php
@@ -1,7 +1,25 @@
 <?php
 
     class Produto{
-        
+        public $nome;
+        public $preco;
     }
 
+    class ProdutoEletronico extends Produto{
+        public $marca;
+        public function exibirInfo():void{
+            echo "Nome:". $nome;
+            echo "<br>";
+            echo "Preço:". $preco;
+            echo "<br>";
+            echo "Marca:". $marca;
+        }
+    }
+
+    $obj= new ProdutoEletronico();
+    $obj->nome("Iphone 17");
+    $obj->preco("18.000,00");
+    $obj->marca("Apple");
+
+    $obj->exibirInfo();
 ?>
